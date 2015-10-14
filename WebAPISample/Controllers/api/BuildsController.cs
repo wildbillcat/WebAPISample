987d72d95e129748aad52cfdcfec8c1dbcc0b340
Patrick McMorran <patrick.mcmorran@aig.com>
2015-10-14 18:28:03 -0400

    First Scaffold without the Javascript

---
+++ WebAPISample/Controllers/api/BuildsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPISample.Controllers.api
+{
+    public class BuildsController : ApiController
+    {
+        // GET: api/Builds
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { " " };
+        }
+
+        // GET: /api/Builds/?Project=
+        public IEnumerable<string> Get(string Project)
+        {
+            string[] BuildList;
+            switch (Project)
+            {
+                case "Proj":
+                    BuildList = new string[] { "Build2", "Build3" };
+                    break;
+                case "value2":
+                    BuildList = new string[] { "Build5", "Build6" };
+                    break;
+                case "Proj2":
+                    BuildList = new string[] { "Build8", "Build9" };
+                    break;
+                case "value666":
+                    BuildList = new string[] { "Build66666", "Build99999" };
+                    break;
+                default:
+                    BuildList = new string[] { "" };
+                    break;
+            }
+
+            return BuildList;
+        }
+
+        // POST: api/Builds
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Builds/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Builds/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPISample/Controllers/api/ProjController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPISample.Controllers.api
+{
+    public class ProjController : ApiController
+    {
+        // GET: api/Test
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { " " };
+        }
+
+        // GET: /api/Proj/?Collection=
+        public IEnumerable<string> Get(string Collection)
+        {
+            string[] ProjList;
+            switch (Collection)
+            {
+                case "test" :
+                    ProjList = new string[] { "Proj", "value2" };
+                    break;
+                case "test2":
+                    ProjList = new string[] { "Proj2", "value666" };
+                    break;
+                default:
+                    ProjList = new string[] { "" };
+                    break;
+            }
+
+            return ProjList;
+        }
+
+        // POST: api/Test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
